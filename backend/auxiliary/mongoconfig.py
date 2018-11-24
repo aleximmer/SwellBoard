b5e6b1bfe17a7ae39478e59f627ebeaba0d8defa
nunomota <lostintime@nunomota.io>
2018-11-24 20:46:54 +0100

    Fixes mongoconfig configuration

--- backend/auxiliary/mongoconfig.py
+++ backend/auxiliary/mongoconfig.py
@@ -9,7 +9,7 @@ class MongoConfig:
     __pass = None
 
     def __init__(self):
-        config = self.__read_config('.server_config.json')
+        config = self.__read_config('auxiliary/.server_config.json')
         self.__host = config.get('host')
         self.__port = config.get('port')
         self.__name = config.get('name')
@@ -17,9 +17,12 @@ class MongoConfig:
         self.__pass = config.get('pass')
 
     def __read_config(self, json_config):
-        return json.load(json_config)
+        data = None
+        with open(json_config) as file:
+            data = json.load(file)
+        return data
 
-    def get_vaues(self):
+    def get_values(self):
         return {
             'host': self.__host,
             'port': self.__port,
